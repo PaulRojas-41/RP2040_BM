1a1d4f0aa95e8c80116c826fbe919a20dd1135a2
Paul Rojas <sirpaul.tapia@hotmail.com>
2026-09-02 00:51:01 +0200

    [I2C0_driver_dev] I2C0 peripheral config and LCD 16x2 integration
    - This commit contains:
    
    	* clean up code and features described below for drv_i2c.c file:
    	* INTR_STATUS bit control status register for bus protection
    	* macros for trigger STOP condition and check bit control in
    	  INTR_STATUS register
    	* reset_sys(void) temporarly commented as behavior is corrupted
    	  when added it
    	* i2c0_byte_tx method for send data to LCD
    	* New values calculated for lcnt and hcnt counters based on 400kbps baudrate and 125MHZ clk_sys
    
    	* Integration of i2c0_byte_tx to LCD driver functions: lcd_i2c.c
    	  file
    
    - test:
    
    	1. first test i2c_byte_tx() connected to LCD library
    	2. keep using SDK gpio and i2c peripheral configuration
    	3. add a messages, reboot and flash
    
    	1. second test: if previous steps works, we can use our own gpio
    	   initialization and i2c0 peripheral configuration

--- comm_protocols/lcd_i2c/lcd_i2c.c
+++ comm_protocols/lcd_i2c/lcd_i2c.c
@@ -8,25 +8,159 @@
 #include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
-#include "pico/binary_info.h"
 #include "hardware/structs/sio.h"
 #include "hardware/structs/iobank0.h"
 #include "hardware/clocks.h"
 #include <stdint.h>
 #include "header/drv_i2c.h"
 
-int main(void) 
+
+/* Example code to drive a 16x2 LCD panel via a I2C bridge chip (e.g. PCF8574)
+
+   NOTE: The panel must be capable of being driven at 3.3v NOT 5v. The Pico
+   GPIO (and therefore I2C) cannot be used at 5v.
+
+   You will need to use a level shifter on the I2C lines if you want to run the
+   board at 5v.
+
+   Connections on Raspberry Pi Pico board, other boards may vary.
+
+   GPIO 4 (pin 6)-> SDA on LCD bridge board
+   GPIO 5 (pin 7)-> SCL on LCD bridge board
+   3.3v (pin 36) -> VCC on LCD bridge board
+   GND (pin 38)  -> GND on LCD bridge board
+*/
+// commands
+const int LCD_CLEARDISPLAY = 0x01;
+const int LCD_RETURNHOME = 0x02;
+const int LCD_ENTRYMODESET = 0x04;
+const int LCD_DISPLAYCONTROL = 0x08;
+const int LCD_CURSORSHIFT = 0x10;
+const int LCD_FUNCTIONSET = 0x20;
+const int LCD_SETCGRAMADDR = 0x40;
+const int LCD_SETDDRAMADDR = 0x80;
+
+// flags for display entry mode
+const int LCD_ENTRYSHIFTINCREMENT = 0x01;
+const int LCD_ENTRYLEFT = 0x02;
+
+// flags for display and cursor control
+const int LCD_BLINKON = 0x01;
+const int LCD_CURSORON = 0x02;
+const int LCD_DISPLAYON = 0x04;
+
+// flags for display and cursor shift
+const int LCD_MOVERIGHT = 0x04;
+const int LCD_DISPLAYMOVE = 0x08;
+
+// flags for function set
+const int LCD_5x10DOTS = 0x04;
+const int LCD_2LINE = 0x08;
+const int LCD_8BITMODE = 0x10;
+
+// flag for backlight control
+const int LCD_BACKLIGHT = 0x08;
+
+const int LCD_ENABLE_BIT = 0x04;
+
+// By default these LCD display drivers are on bus address 0x27
+static int addr = 0x27;
+
+// Modes for lcd_send_byte
+#define LCD_CHARACTER  1
+#define LCD_COMMAND    0
+
+#define MAX_LINES      2
+#define MAX_CHARS      16
+
+#define DELAY_US 600
+
+void lcd_i2c0_write_byte(uint8_t val)
 {
-    stdio_init_all();
-    setupClocks();
+    i2c0_byte_tx(&val,1);
+}
+
+void lcd_toggle_enable(uint8_t val) 
+{
+    sleep_us(DELAY_US);
+    lcd_i2c0_write_byte(val | LCD_ENABLE_BIT);
+    sleep_us(DELAY_US);
+    lcd_i2c0_write_byte(val & ~LCD_ENABLE_BIT);
+    sleep_us(DELAY_US);
+}
+
+// The display is sent a byte as two separate nibble transfers
+void lcd_send_byte(uint8_t val, int mode) 
+{
+    uint8_t high = mode | (val & 0xF0) | LCD_BACKLIGHT;
+    uint8_t low = mode | ((val << 4) & 0xF0) | LCD_BACKLIGHT;
+
+    lcd_i2c0_write_byte(high);
+    lcd_toggle_enable(high);
+    lcd_i2c0_write_byte(low);
+    lcd_toggle_enable(low);
+}
+
+void lcd_clear(void) 
+{
+    lcd_send_byte(LCD_CLEARDISPLAY, LCD_COMMAND);
+}
+
+// go to location on LCD
+void lcd_set_cursor(int line, int position) 
+{
+    int val = (line == 0) ? 0x80 + position : 0xC0 + position;
+    lcd_send_byte(val, LCD_COMMAND);
+}
+
+static inline void lcd_char(char val) 
+{
+    lcd_send_byte(val, LCD_CHARACTER);
+}
+
+void lcd_string(const char *s) 
+{
+    while (*s) {
+        lcd_char(*s++);
+    }
+}
+
+void lcd_init() 
+{
+    lcd_send_byte(0x03, LCD_COMMAND);
+    lcd_send_byte(0x03, LCD_COMMAND);
+    lcd_send_byte(0x03, LCD_COMMAND);
+    lcd_send_byte(0x02, LCD_COMMAND);
+
+    lcd_send_byte(LCD_ENTRYMODESET | LCD_ENTRYLEFT, LCD_COMMAND);
+    lcd_send_byte(LCD_FUNCTIONSET | LCD_2LINE, LCD_COMMAND);
+    lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND);
+
+    lcd_clear();
+}
+
+int main() 
+{
     i2c0_init_peripheral();
     gpio_i2c0_init();
 
-    //lcd_init(&my_display, i2c0_byte_tx, 0x27);
+    lcd_init();
+
+    static char *message[] = {"Hola", "RP2040", "Cortex M0","Bare Metal",
+                            "Design", "I2C0 BUS", "NO DEBUGGER",
+                            "LCD", "VSCODE", "CMAKE","NO SDK"};
 
     while (1) 
     {
-        test_i2c0();
-        for(int i = 0; i < 10000000; i++);
+        for (uint m = 0; m < sizeof(message) / sizeof(message[0]); m += MAX_LINES) 
+        {
+            for (int line = 0; line < MAX_LINES; line++) 
+            {
+                lcd_set_cursor(line, (MAX_CHARS / 2) - strlen(message[m + line]) / 2);
+                lcd_string(message[m + line]);
+            }
+            sleep_ms(2000);
+            lcd_clear();
+        }
     }
 }
